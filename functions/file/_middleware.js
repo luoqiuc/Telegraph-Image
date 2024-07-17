37c30a3d555aea4d1d1306c1ada40a619348fdf8
luoqiuc <43166924+luoqiuc@users.noreply.github.com>
2024-07-17 10:14:18 +0800

    取消添加防盗链

--- functions/file/_middleware.js
+++ functions/file/_middleware.js
@@ -2,8 +2,8 @@ import { errorHandling, telemetryData } from '../utils/middleware';
 
 export const onRequest = [errorHandling, telemetryData];
 
-let Referer = request.headers.get('Referer') || "Referer"
-let refererUrl = new URL(Referer)
-if(!allowedDomains.includes(refererUrl.hostname)){
-    return Response.redirect("https://img.131213.xyz/asset/image/blocked.png", 302);
-}
+// let Referer = request.headers.get('Referer') || "Referer"
+// let refererUrl = new URL(Referer)
+// if(!allowedDomains.includes(refererUrl.hostname)){
+//     return Response.redirect("https://img.131213.xyz/asset/image/blocked.png", 302);
+// }
